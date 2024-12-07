52d2f2068b1b65cfdcf3875f9df3e8109bafd985
Elias <eliasmazo@hotmail.com>
2024-12-07 10:31:37 -0500

    add: Adding files

--- js/fuction.js
+++ js/fuction.js
@@ -25,25 +25,13 @@ function campamento() {
 
 const button = document.querySelector('.burguer')
 const Burguer_nav = document.querySelector('.Burguer_nav')
-const header = document.querySelector('.header')
+const nav = document.querySelector('.nav')
 
 button.addEventListener('click', () => {
-    const scrollY = window.scrollY; // Guarda la posición actual del scroll
-    document.body.style.position = 'fixed';
-    document.body.style.width = '-webkit-fill-available';
-    document.body.style.top = `-${0}px`;
-
-    header.style.display = 'none'
     Burguer_nav.style.display = 'flex'
 })
 
 Burguer_nav.addEventListener('click', () => {
-    document.body.style.position = '';
-    document.body.style.top = '';
-    window.scrollTo(0, parseInt('0'));
-
-
     document.body.style.width = 'auto';
-    header.style.display = 'flex'
     Burguer_nav.style.display = 'none'
 })
